d321c270a70c6ee4d83a81ca617d1222cb20d94e
QuillanW <56843492+QuillanW@users.noreply.github.com>
2021-12-14 11:22:24 +0100

    Bestel pagina

---
+++ js/products.js
@@ -0,0 +1,80 @@
+var products = {
+  123: {
+    name : "Hamburger",
+    desc : "Standaard hamburger menu.",
+    img : "burger.png",
+    price : 8.99
+  },
+  124: {
+    name : "Friet",
+    desc : "Heerlijke zelfgemaakte friet.",
+    img : "friet.png",
+    price : 3.99
+  },
+  125: {
+    name: "Friet",
+    desc: "Heerlijke zelfgemaakte friet.",
+    img: "friet.png",
+    price: 3.99
+  },
+  126: {
+    name: "Friet",
+    desc: "Heerlijke zelfgemaakte friet.",
+    img: "friet.png",
+    price: 3.99
+  },
+  127: {
+    name: "Friet",
+    desc: "Heerlijke zelfgemaakte friet.",
+    img: "friet.png",
+    price: 3.99
+  },
+  128: {
+    name: "Friet",
+    desc: "Heerlijke zelfgemaakte friet.",
+    img: "friet.png",
+    price: 3.99
+  },
+  129: {
+    name: "Friet",
+    desc: "Heerlijke zelfgemaakte friet.",
+    img: "friet.png",
+    price: 3.99
+  },
+  130: {
+    name: "Friet",
+    desc: "Heerlijke zelfgemaakte friet.",
+    img: "friet.png",
+    price: 3.99
+  },
+  131: {
+    name: "Friet",
+    desc: "Heerlijke zelfgemaakte friet.",
+    img: "friet.png",
+    price: 3.99
+  },
+  132: {
+    name: "Friet",
+    desc: "Heerlijke zelfgemaakte friet.",
+    img: "friet.png",
+    price: 3.99
+  },
+  133: {
+    name: "Friet",
+    desc: "Heerlijke zelfgemaakte friet.",
+    img: "friet.png",
+    price: 3.99
+  },
+  134: {
+    name: "Friet",
+    desc: "Heerlijke zelfgemaakte friet.",
+    img: "friet.png",
+    price: 3.99
+  },
+  135: {
+    name: "Friet",
+    desc: "Heerlijke zelfgemaakte friet.",
+    img: "friet.png",
+    price: 3.99
+  }
+};
